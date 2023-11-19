4722353e09c44d772ace486db502214a40921d63
muhammadarif2113 <arifmuhammad2113@gmail.com>
2023-11-18 21:23:40 -0500

    t

--- moscow/next.config.js
+++ moscow/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  forceSwcTransforms: true,
+  swcMinify: false
 }
 
 module.exports = nextConfig
